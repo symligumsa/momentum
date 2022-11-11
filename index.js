94de8a0efe1b911669daee0100b6aea00cb9f7ab
symligumsa <117967945+symligumsa@users.noreply.github.com>
2022-11-11 14:39:05 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    renderTime()
+    renderQuote()
+});
+//현재 시간
+function renderTime() {
+    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let datetime = data['datetime'].substr(11,5)
+            $('#time').text(datetime)
+        })
+}
+//명언
+function renderQuote() {
+    let url = `https://api.quotable.io/random`
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`
+            let author = `- ${data['author']} -`
+            $('#content').text(content)
+            $('#author').text(author)
+        })
+}
